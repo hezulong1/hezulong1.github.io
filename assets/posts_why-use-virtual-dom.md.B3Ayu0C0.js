8e7cd95b01a375b5636e7ecab330cca90e2c0f2b
hezulong1 <hezulong1@users.noreply.github.com>
2026-03-04 05:49:35 +0000

    deploy: da4c880340a612f3681672da61f3e7af3836291d

--- assets/posts_why-use-virtual-dom.md.B3Ayu0C0.js
+++ assets/posts_why-use-virtual-dom.md.B3Ayu0C0.js
@@ -1,5 +1,5 @@
-import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode } from "./app.Dm0xyOCi.js";
-const __pageData = JSON.parse('{"title":"为什么使用虚拟 DOM ？","description":"","frontmatter":{"title":"为什么使用虚拟 DOM ？","date":"2026-03-04T10:52","layout":"post"},"headers":[],"relativePath":"posts/why-use-virtual-dom.md","filePath":"posts/why-use-virtual-dom.md","lastUpdated":1772603207000}');
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode } from "./app.0BlytyvY.js";
+const __pageData = JSON.parse('{"title":"为什么使用虚拟 DOM","description":"","frontmatter":{"title":"为什么使用虚拟 DOM","date":"2026-03-04T10:52","layout":"post"},"headers":[],"relativePath":"posts/why-use-virtual-dom.md","filePath":"posts/why-use-virtual-dom.md","lastUpdated":1772603285000}');
 const _sfc_main = { name: "posts/why-use-virtual-dom.md" };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
