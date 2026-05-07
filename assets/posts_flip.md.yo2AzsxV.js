acd3a01e49e88535f8adbcbb3e172b508774d5ae
hezulong1 <hezulong1@users.noreply.github.com>
2026-05-07 02:22:06 +0000

    deploy: 9cd7c04822c55751312da869b3f290224365fd7e

---
+++ assets/posts_flip.md.yo2AzsxV.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, b as createBaseVNode } from "./app.Kfzz42Xn.js";
+const __pageData = JSON.parse('{"title":"FLIP 动画","description":"","frontmatter":{"title":"FLIP 动画","date":"2026-04-30T16:28","layout":"post"},"headers":[],"relativePath":"posts/flip.md","filePath":"posts/flip.md","lastUpdated":1778120488000}');
+const _sfc_main = { name: "posts/flip.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createBaseVNode("p", null, "传", -1)
+  ])]);
+}
+const flip = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  flip as default
+};
