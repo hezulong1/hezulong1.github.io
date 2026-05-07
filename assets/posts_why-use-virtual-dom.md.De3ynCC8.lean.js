b1f6b98d4753837cc6282bad9f95510401abaca2
hezulong1 <hezulong1@users.noreply.github.com>
2026-05-07 07:13:23 +0000

    deploy: 9a4a1168cf86e9cc0d1e5dc74354ae1f4ab8fed2

---
+++ assets/posts_why-use-virtual-dom.md.De3ynCC8.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode } from "./app.C78-U9VV.js";
+const __pageData = JSON.parse('{"title":"为什么使用虚拟 DOM","description":"","frontmatter":{"title":"为什么使用虚拟 DOM","date":"2026-03-04T10:52","update_date":"2026-03-11T16:29","layout":"post"},"headers":[],"relativePath":"posts/why-use-virtual-dom.md","filePath":"posts/why-use-virtual-dom.md","lastUpdated":1778137978000}');
+const _sfc_main = { name: "posts/why-use-virtual-dom.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 26)
+  ])]);
+}
+const whyUseVirtualDom = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  whyUseVirtualDom as default
+};
