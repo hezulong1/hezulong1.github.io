728ff70ded956a01688810cb4d99c2877895093d
hezulong1 <hezulong1@users.noreply.github.com>
2026-05-07 02:01:08 +0000

    deploy: 7e0ec13418fc795ad111000477abfdc3ddb64e19

---
+++ assets/index.md.COGqDP-8.js
@@ -0,0 +1,17 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, b as createBaseVNode, d as createTextVNode } from "./app.JgaK4tEy.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{"isHome":true},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":1778119247000}');
+const _sfc_main = { name: "index.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createBaseVNode("p", null, [
+      createTextVNode("哈喽，很高兴认识"),
+      createBaseVNode("strong", null, "你"),
+      createTextVNode("！")
+    ], -1)
+  ])]);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
---
+++ assets/posts_flip.md.Bdp3W8Ky.lean.js
@@ -0,0 +1,18 @@
+import { _ as _export_sfc, r as resolveComponent, o as openBlock, c as createElementBlock, e as createVNode, a as createStaticVNode } from "./app.JgaK4tEy.js";
+const __pageData = JSON.parse('{"title":"FLIP 动画","description":"","frontmatter":{"title":"FLIP 动画","date":"2026-04-30T16:28","layout":"post"},"headers":[],"relativePath":"posts/flip.md","filePath":"posts/flip.md","lastUpdated":1778119247000}');
+const _sfc_main = { name: "posts/flip.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_OpenLink = resolveComponent("OpenLink");
+  return openBlock(), createElementBlock("div", null, [
+    createVNode(_component_OpenLink, {
+      href: "https://aerotwist.com/blog/flip-your-animations/",
+      label: "FLIP Your Animations"
+    }),
+    _cache[0] || (_cache[0] = createStaticVNode("", 10))
+  ]);
+}
+const flip = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  flip as default
+};
