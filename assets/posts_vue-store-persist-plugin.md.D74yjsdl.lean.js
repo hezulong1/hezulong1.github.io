f365ee325e0341ce051f25faf8bd129f163da75f
hezulong1 <hezulong1@users.noreply.github.com>
2026-09-13 15:45:57 +0000

    deploy: eac7bbc74eb922c852faff2096709a86dfb40932

---
+++ assets/posts_vue-store-persist-plugin.md.D74yjsdl.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createStaticVNode } from "./app.DcmFuC7l.js";
+const __pageData = JSON.parse('{"title":"Vue 状态仓库持久化","description":"","frontmatter":{"title":"Vue 状态仓库持久化","date":"2026-09-13T22:42","layout":"post"},"headers":[],"relativePath":"posts/vue-store-persist-plugin.md","filePath":"posts/vue-store-persist-plugin.md","lastUpdated":1789314321000}');
+const _sfc_main = { name: "posts/vue-store-persist-plugin.md" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, [..._cache[0] || (_cache[0] = [
+    createStaticVNode("", 11)
+  ])]);
+}
+const vueStorePersistPlugin = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  vueStorePersistPlugin as default
+};
